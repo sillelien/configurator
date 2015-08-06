1e13f3eaa0656ea73922565e8121eea7bf70d708
neilellis <hello@neilellis.me>
2015-08-07 00:10:50 +0100

    Docs

--- server/client_script.sh
+++ server/client_script.sh
@@ -1,18 +1,13 @@
-git config --global user.email "configurator@$(hostname)"
-git config --global user.name "Configurator @ $(hostname)"
 
-set -x
+if [ ! -d /config/.git ]
+then
+   git clone git://${SCSERVER_PORT_9418_TCP_ADDR}:${SCSERVER_PORT_9418_TCP_PORT}/config /config
+fi
+
+cd /config
 
 while true
 do
-
-    if [ ! -d /config/.git ]
-    then
-       git clone git://${SCSERVER_PORT_9418_TCP_ADDR}:${SCSERVER_PORT_9418_TCP_PORT}/config /config
-    fi
-    cd /config
     git pull origin master
     sleep 10
-
-
 done
